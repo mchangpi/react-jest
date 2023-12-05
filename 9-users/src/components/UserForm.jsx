163023fd450270956818f294e89a5f0be1ddda49
Milton Chang <mchangpi@gmail.com>
2023-12-05 17:21:43 +0800

    L18 Recording Function Calls

--- 9-users/src/components/UserForm.jsx
+++ 9-users/src/components/UserForm.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 
 function UserForm({ onUserAdd }) {
-  const [email, setEmail] = useState('');
   const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
--- 9-users/src/tests/UserForm.test.js
+++ 9-users/src/tests/UserForm.test.js
@@ -19,20 +19,38 @@ test('Shows 2 inputs and 1 button', () => {
   expect(button).toBeInTheDocument();
 });
 
-test('Calls onUserAdd when the form is submitted', () => {
-  render(<UserForm />);
+test('Calls onUserAdd when the form is submitted', async () => {
+  // NOT the best implementation!
+  const argList = [];
+  const callback = (...args) => {
+    argList.push(args);
+  };
+
+  render(<UserForm onUserAdd={callback} />);
 
   // 1.find the two inputs
   const [nameInput, emailInput] = screen.getAllByRole('textbox');
+  // screen.debug(nameInput);
+  // screen.debug(emailInput);
 
   // 2.type in a name
-  user.click(nameInput);
-  user.keyboard('milton');
+  await user.click(nameInput);
+  await user.keyboard('milton');
 
   // 3.type in an email
-  user.click(emailInput);
-  user.keyboard('milton@gmail.com');
+  await user.click(emailInput);
+  await user.keyboard('milton@gmail.com');
+
   // 4.find the button
+  const button = screen.getByRole('button');
+  // screen.debug(button);
+
   // 5.click the button
+  await user.click(button);
+  console.log(argList);
+  console.log(argList[0][0]);
+
   // 6.Assertion to make sure 'onUserAdd' gets called with email/name
+  expect(argList).toHaveLength(1);
+  expect(argList[0][0]).toEqual({ name: 'milton', email: 'milton@gmail.com' });
 });
