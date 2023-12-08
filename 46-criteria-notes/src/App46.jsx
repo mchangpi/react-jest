54b371b075fb78b0d722aa33549bb6da621422de
Milton Chang <mchangpi@gmail.com>
2023-12-08 16:26:03 +0800

    L46 Query Criteria

---
+++ 46-criteria-notes/src/App46.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './css/output.css';
+
+function App() {
+  const [email, setEmail] = useState('milton@gmail.com');
+
+  return (
+    <form className="flex max-w-xs flex-col space-x-2 space-y-4">
+      <h4>Please Enter Data</h4>
+
+      <div data-testid="image wrapper">
+        <img alt="data" src="./data.jpg" />
+      </div>
+
+      <label htmlFor="email">Email</label>
+      <input
+        id="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="border-2"
+      ></input>
+
+      <label htmlFor="color">Color</label>
+      <input id="color" placeholder="red" className="border-2 p-2" />
+
+      <button
+        title="Click and Submit"
+        className="rounded border-2 bg-stone-100 p-2"
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default App;
