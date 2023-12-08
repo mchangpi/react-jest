b1bff32f97594c3170f54c3c867791d90ea2f9ba
Milton Chang <mchangpi@gmail.com>
2023-12-08 22:59:50 +0800

    L47 When to Use Each Suffix

--- 46-criteria-notes/src/App46.jsx
+++ 46-criteria-notes/src/App46.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <form className="flex max-w-xs flex-col space-x-2 space-y-4">
-      <h4>Please Enter Data</h4>
+      <h4>Enter Data</h4>
 
       <div data-testid="image wrapper">
         <img alt="data" src="./data.jpg" />
@@ -21,7 +21,7 @@ function App() {
       ></input>
 
       <label htmlFor="color">Color</label>
-      <input id="color" placeholder="red" className="border-2 p-2" />
+      <input id="color" placeholder="Red" className="border-2 p-2" />
 
       <button
         title="Click and Submit"
--- 46-criteria-notes/src/tests/App46.test.js
+++ 46-criteria-notes/src/tests/App46.test.js
@@ -6,5 +6,23 @@ import App from '../App46';
 test('Select different elements', async () => {
   render(<App />);
 
-  const elements = [];
+  const elements = [
+    screen.getByRole('button'),
+    screen.getByText('Enter Data'),
+    screen.getByText(/enter/i),
+
+    screen.getByLabelText('Email'),
+    screen.getByPlaceholderText('Red'),
+    screen.getByDisplayValue('milton@gmail.com'),
+    screen.getByAltText('data'),
+    screen.getByTitle(/click and submit/i),
+
+    screen.getByTestId(/image wrapper/i),
+  ];
+
+  for (let element of elements) {
+    expect(element).toBeInTheDocument();
+  }
+
+  // screen.debug(screen.getByLabelText('Email'));
 });
