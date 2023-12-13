8fe927da05db4508dfd2b5d10d036e867f9f6dcf
Milton Chang <mchangpi@gmail.com>
2023-12-13 17:47:51 +0800

    L55 The Path Ahead

---
+++ 56-codesplain-starter/src/components/forms/Label.js
@@ -0,0 +1,17 @@
+import React from "react";
+import classNames from "classnames";
+
+function Label({ children, className, ...props }) {
+  const classes = classNames(
+    "block text-sm font-medium text-gray-700",
+    className
+  );
+
+  return (
+    <label {...props} className={classes}>
+      {children}
+    </label>
+  );
+}
+
+export default Label;
